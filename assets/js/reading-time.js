0ae16b944fb05c5a2b879d7c219a3866508a62c4
Anton Korepanov <tony1000.dev@ya.ru>
2016-09-11 02:46:13 +0500

    remove all

--- assets/js/reading-time.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    var txt = $('.content')[0].textContent;
-    wordCount = txt.split( /\s+/ ).length;
-    var readingTimeInMinutes = Math.floor(wordCount / 200) + 1;
-    var readingTimeAsString = readingTimeInMinutes + " min";
-    $('article .reading-time').html(readingTimeAsString);
-});
